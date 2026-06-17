7c178618963d8323aaec92ae454393f3a42fb33f
srikanthpragada <srikanthpragada@gmail.com>
2026-06-17 17:23:23 +0530

    17-JUN

---
+++ square.c
@@ -0,0 +1,15 @@
+// print square of a number
+
+#include <stdio.h>
+
+void main()
+{
+  int num, square;
+
+     num = 10;
+     square = num * num;
+
+     //printf("Square = %d", square);
+     printf("Square of %d is %d", num, square);
+
+}
