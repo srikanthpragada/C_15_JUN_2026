d86fc70930d74917c352030b0bb48b1e1e27fe15
srikanthpragada <srikanthpragada@gmail.com>
2026-06-17 17:07:59 +0530

    17-JUN

---
+++ contact.c
@@ -0,0 +1,10 @@
+// Program to display contact details
+
+
+#include <stdio.h>
+
+void main()
+{
+    printf("contact@srikanthtechnologies.com\n\n");
+    printf("9059057000\n");
+}
---
+++ hello.c
@@ -0,0 +1,10 @@
+// My first C program
+// Date : 17-JUN-2026
+
+
+#include <stdio.h>
+
+void main()
+{
+    printf("Hello World!");
+}
